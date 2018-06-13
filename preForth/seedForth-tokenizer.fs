b50362605c828a668c16ba564fe2e9d891e362e1
Ulrich Hoffmann <uho@xlerb.de>
2018-06-13 19:17:21 +0200

    Add tester to seedForth

--- preForth/seedForth-tokenizer.fs
+++ preForth/seedForth-tokenizer.fs
@@ -32,7 +32,7 @@ FUN: negate    FUN: +           FUN: 0=        FUN: ?dup		\ 14 15 16 17
 FUN: cells     FUN: +!          FUN: h@        FUN: h,			\ 18 19 1A 1B
 FUN: here      FUN: allot       FUN: ,         FUN: c,			\ 1C 1D 1E 1F
 FUN: fun       FUN: interpreter FUN: compiler  FUN: create		\ 20 21 22 23
-FUN: does>     FUN: cold										\ 24 25
+FUN: does>     FUN: cold        FUN: depth				\ 24 25 26
 
 : [ ( -- )  0 SUBMIT ;
 : ] ( -- )  compiler ;
