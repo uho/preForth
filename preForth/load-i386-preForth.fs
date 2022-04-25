4add97d48042d4a047c36cfcb9966132d65e5c1a
Nick Downing <nick@ndcode.org>
2022-04-25 12:20:13 +1000

    Simplify bootstrapping by not creating a new wordlist and only defining the nonstandard words on top of gForth's standard words, produces redefinition warnings

--- preForth/load-i386-preForth.fs
+++ preForth/load-i386-preForth.fs
@@ -1,12 +1,12 @@
 \ load i386 preForth on top of a host Forth system
+\ omit the standard words (either machine code or high level code)
 
-include load-preForth.fs
-include preForth-i386-rts.pre
+include preForth-bootstrap.fs
+\ include preForth-i386-rts.pre
 include preForth-rts-nonstandard.pre
-include preForth-rts.pre
+\ include preForth-rts.pre
 include preForth-i386-backend.pre
 include preForth.pre
 
 cold
-
 bye
